ac37c950fca0ce0639edd82de062fd32ed307d76
YaotianLu <yl5856@columbia.edu>
2025-10-05 12:03:48 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,7 +1,6 @@
 
- def getBondPrice(y, face, couponRate, m, ppy=1):
-    n = m * ppy
-    r = y / ppy
-    c = face * couponRate / ppy
-    bondPrice = c * (1 - (1 + r) ** -n) / r + face * (1 + r) ** -n
-    return bondPrice
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    n = int(m * ppy)
+    r = float(y) / float(ppy)
+    c = float(face) * float(couponRate) / float(ppy)
+    return c * (1 - (1 + r) ** -n) / r + face * (1 + r) ** -n
