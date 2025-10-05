327ca6736ba224211fe273d31bef390059c70681
YaotianLu <yl5856@columbia.edu>
2025-10-05 12:44:26 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,10 +1,7 @@
-
-
 def getBondPrice_E(face, couponRate, yc):
-    c = float(face) * float(couponRate)
-    bondprice = 0.0
-    for t, y in enumerate(yc, start=1):
-        price += c / (1 + float(y))**t
-    bondprice += float(face) / (1 + float(yc[-1]))**len(yc)
-    return bondprice
-
+    face = float(face)
+    couponRate = float(couponRate)
+    c = face * couponRate
+    price = sum(c / (1 + float(y)) ** (t + 1) for t, y in enumerate(yc))
+    price += face / (1 + float(yc[-1])) ** len(yc)
+    return price
