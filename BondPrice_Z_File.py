7dde85397422e16edd94d71cdce917bee0b0fcb5
YaotianLu <yl5856@columbia.edu>
2025-10-05 12:50:46 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,7 @@
-
-
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    if len(times) != len(yc):
+        raise ValueError("times and yc must have same length")
+    c = float(face) * float(couponRate)
+    price = sum(c / (1 + float(y))**float(t) for t, y in zip(times, yc))
+    price += float(face) / (1 + float(yc[-1]))**float(times[-1])
+    return price
